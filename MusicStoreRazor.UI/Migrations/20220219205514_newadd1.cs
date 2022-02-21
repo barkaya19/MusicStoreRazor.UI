a5628ff89c281ce66d27f61684a7d99389868d67
b-kay <b-kay@DESKTOP-TS4E8J1>
2022-02-21 21:25:24 +0300

    Add project files.

---
+++ MusicStoreRazor.UI/Migrations/20220219205514_newadd1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicStoreRazor.UI.Migrations
+{
+    public partial class newadd1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BarcodeNumber",
+                table: "Musics",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BarcodeNumber",
+                table: "Musics");
+        }
+    }
+}
---
+++ MusicStoreRazor.UI/Models/MusicStoreListContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStoreRazor.UI.Models
+{
+    public class MusicStoreListContext:DbContext
+    {
+        public MusicStoreListContext(DbContextOptions<MusicStoreListContext> options):base(options)
+        {
+
+        }
+        public DbSet<Music> Musics { get; set; }
+    }
+}
